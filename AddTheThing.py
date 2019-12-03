5ba29032377d7feb6a4d65c7214905a42a14ffef
noahj13 <noahjarvis0@gmail.com>
2019-12-03 12:42:34 -0500

    updated CSV files

--- AddTheThing.py
+++ AddTheThing.py
@@ -1,4 +1,4 @@
-import mysql.connector
+import mysql.connectorMatth
 conn = mysql.connector.connect(user='root',
                                password='ThisIsASecurePassword1313',
                                host='localhost',
